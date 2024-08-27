914ba24dc31670819d2992c7394feba807ffbd4f
Ethan <ethan1022@gmail.com>
2024-08-27 22:34:20 +0900

    Initial Commit

---
+++ GithubApiPlayground/GithubApiPlaygroundApp.swift
@@ -0,0 +1,20 @@
+//
+//  GithubApiPlaygroundApp.swift
+//  GithubApiPlayground
+//
+//  Created by Ethan Wen on 2024/8/27.
+//
+
+import SwiftUI
+
+@main
+struct GithubApiPlaygroundApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
